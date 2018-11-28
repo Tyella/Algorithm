2eb23212a6aa761bc5535e8a7c779a1d0cf17cd6
Tyella <2720711917@qq.com>
2018-11-28 23:20:32 +0800

    Update String.java

--- String.java
+++ String.java
@@ -17,4 +17,18 @@ public class String {
         return ans;
 		
 	}
+	
+	public ListNode reverseList(ListNode head) {
+            	if(head==null || head.next==null)
+    		return head;
+    	ListNode pcur=head;
+    	ListNode prev=null;
+    	while(pcur!=null) {
+    		ListNode ptmp=pcur;
+    		pcur=pcur.next;
+    		ptmp.next=prev;
+    		prev=ptmp;
+    	}
+    	return prev;
+    }
 }
