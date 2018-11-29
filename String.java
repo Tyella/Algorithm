53bbfb9a7596048afa8eff2e73c9e8057b7dfc3b
Tyella <2720711917@qq.com>
2018-11-29 22:50:34 +0800

    Update String.java

--- String.java
+++ String.java
@@ -31,4 +31,18 @@ public class String {
     	}
     	return prev;
     }
+	
+	public int mySqrt(int x) {
+		int low=0;
+		int high=x;
+		while(low<=high) {
+			long mid=(long)(low+high)/2;
+			if(mid*mid>x)
+				high=(int)mid-1;
+			else if(mid*mid<x)
+				low=(int)mid+1;
+			else
+				return (int)mid;
+		}
+		return high;
 }
