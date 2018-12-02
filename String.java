ed924b2ff378da49f3b8bb3e0652f959ad4f02c7
Tyella <2720711917@qq.com>
2018-12-02 23:29:14 +0800

    Update String.java

--- String.java
+++ String.java
@@ -58,3 +58,21 @@ public class String {
     	   }
        return max;
     }
+	
+	    public int firstUniqChar(String s) {
+        Map<Character,Integer> map=new LinkedHashMap<>();
+		char[] str=s.toCharArray();
+		for(int i=0;i<str.length;i++) {
+			if(!map.containsKey(str[i])) {
+				map.put(str[i], i);
+			}
+			else {
+				map.put(str[i], -1);
+			}
+		}
+		for(Map.Entry<Character, Integer> entry:map.entrySet()) {
+			if(entry.getValue()!=-1)
+				return entry.getValue();
+		}
+		return -1;
+    }
