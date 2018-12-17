1598c888fc1dc1ef3c12d385751aecea08b19e14
Tyella <2720711917@qq.com>
2018-12-17 20:05:54 +0800

    LRU缓存

---
+++ LRUCache.java
@@ -0,0 +1,27 @@
+package Algorithm;
+
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+class LRUCache {
+
+    private final LinkedHashMap<Integer, Integer> cache;
+
+    public LRUCache(int capacity) {
+        cache = new LinkedHashMap<Integer, Integer>(capacity, 0.75f, true) {
+            @Override
+            protected boolean removeEldestEntry(Map.Entry eldest) {
+                return size() > capacity;
+            }
+        };
+    }
+
+    public int get(int key) {
+        return cache.getOrDefault(key, -1);
+    }
+
+    public void put(int key, int value) {
+        cache.put(key, value);
+    }
+}
