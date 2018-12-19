689c0742fb579b11783e7978a1d8695e7abc7915
Tyella <2720711917@qq.com>
2018-12-19 22:25:24 +0800

    单链表反转

--- List.java
+++ List.java
@@ -1,5 +1,23 @@
 package com.example.demo.dao;
 
+ class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) { val = x; }
+  }
 public class List {
     //test leetcode
+    public ListNode reverseList(ListNode head) {
+        if(head==null || head.next==null)
+            return head;
+        ListNode pcur=head;
+        ListNode prev=null;
+        while(pcur!=null) {
+            ListNode ptmp=pcur;
+            pcur=pcur.next;
+            ptmp.next=prev;
+            prev=ptmp;
+        }
+        return prev;
+    }
 }
