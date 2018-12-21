857b0cca0f8937c1ca79aa061a296a2a68755496
Tyella <2720711917@qq.com>
2018-12-21 17:55:30 +0800

    最长连续递增序列

--- Array.java
+++ Array.java
@@ -249,8 +249,26 @@ public class Array {
 		return result;
 	}
 
-	
+	//最长连续递增序列
+	public int findLengthOfLCIS(int[] nums){
+		if(nums.length==0)
+			return 0;
+		int count=1;
+		int max=0;
+		for(int i=0;i<nums.length-1;i++){
+			if(nums[i]<nums[i+1]){
+				count++;
+			}
+			else{
+				max=Math.max(max,count);
+				count=1;
+			}
+		}
+		max=Math.max(max,count);
+		return max;
+	}
 
+	
 
 
 
