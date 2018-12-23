caaf080f04579962b43a605b605f832fe9fbaf92
Tyella <2720711917@qq.com>
2018-12-23 19:33:07 +0800

    寻找旋转排序数组中的最小值II

--- FindMinII.java
+++ FindMinII.java
@@ -1,4 +1,18 @@
 package Algorithm;
 
 public class FindMinII {
+    //寻找旋转排序数组中的最小值 II
+    public int findMinII(int[] nums) {
+        int low = 0, high = nums.length - 1;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] < nums[high])
+                high = mid;
+            else if (nums[mid] > nums[high])
+                low = mid + 1;
+            else
+                high--;
+        }
+        return nums[low];
+    }
 }
