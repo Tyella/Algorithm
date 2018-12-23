ce98e3edfa143fb5cd80d2e95d94c7ee93424ade
Tyella <2720711917@qq.com>
2018-12-23 19:40:00 +0800

    有效的完全平方数

--- IsPerfectSquare.java
+++ IsPerfectSquare.java
@@ -1,4 +1,22 @@
 package Algorithm;
 
 public class IsPerfectSquare {
+    //有效的完全平方数
+    public boolean isPerfectSquare(int num) {
+        if (num == 1 || num == 0)
+            return true;
+        int low = 1;
+        int high = num;
+        while (low + 1 < high) {
+            int mid = low + (high - low) / 2;
+            if (mid * mid == num)
+                return true;
+            //if(mid*mid>num)在有些情况下通不过
+            if (mid > num / mid)
+                high = mid;
+            else
+                low = mid;
+        }
+        return false;
+    }
 }
