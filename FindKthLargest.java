1b9fd2ad8879e7c1cf7ff5c17eecb62bd6960786
Tyella <2720711917@qq.com>
2019-01-03 21:16:43 +0800

    数组中的第k个最大元素

--- FindKthLargest.java
+++ FindKthLargest.java
@@ -1,4 +1,16 @@
 package Algorithm;
 
+import java.util.PriorityQueue;
+
 public class FindKthLargest {
+    public int findKthLargest(int[] nums,int k){
+        PriorityQueue pq=new PriorityQueue(nums.length);
+        for(int i=0;i<nums.length;i++){
+            pq.add(nums[i]);
+        }
+        for(int j=0;j<nums.length-k;j++){
+            pq.poll();
+        }
+        return (int)pq.peek();
+    }
 }
