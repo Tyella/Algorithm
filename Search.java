95853fd2ee7729428ca5b6ccff151d093acc2094
Tyella <2720711917@qq.com>
2019-01-17 22:05:30 +0800

    修改

--- Search.java
+++ Search.java
@@ -1,6 +1,9 @@
 package Algorithm;
 
 public class Search {
+    /*
+    二叉树的变种,找出mid做判断确定相应的low和high值
+     */
     public int search(int[] nums,int target){
         if(nums.length==0)
             return -1;
@@ -10,12 +13,12 @@ public class Search {
             int mid=low+(high-low)/2;
             if(target==nums[mid])
                 return mid;
-            if(nums[mid]>=nums[low]){
+            if(nums[mid]>=nums[low]){     //节点在mid后
                 if(target>=nums[low] && target<nums[mid])
                     high=mid-1;
                 else
                     low=mid+1;
-            }else{
+            }else{                       //节点在mid前
                 if(target>nums[mid] && target<=nums[high])
                     low=mid+1;
                 else
--- ZigzagLeverOrder.java
+++ ZigzagLeverOrder.java
@@ -8,16 +8,16 @@ import java.util.Stack;
 
 public class ZigzagLeverOrder {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        List<List<Integer>> result = new ArrayList();
+        List<List<Integer>> result = new ArrayList<>();
         if(root == null) return result;
         boolean isRight = true;
 
         Queue<TreeNode> q =  new LinkedList<>();
         q.add(root);
         while(!q.isEmpty()){
-            List<Integer> layer = new ArrayList();
+            List<Integer> layer = new ArrayList<>();
             int size = q.size();
-            Stack<TreeNode> s = new Stack();
+            Stack<TreeNode> s = new Stack<>();
             for(int i = 0; i < size; i++){
                 s.push(q.poll());
             }
@@ -35,7 +35,6 @@ public class ZigzagLeverOrder {
             result.add(layer);
             isRight = !isRight;
         }
-
         return result;
     }
 
